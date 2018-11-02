54ca2e26fdfb98dc12bfa39b93eb153185c1d0b8
Ali Alizada <akbar.alizadeh5@gmail.com>
2018-11-01 18:04:23 -0700

    add descriptio to the codes

--- src/containers/gallery.js
+++ src/containers/gallery.js
@@ -7,19 +7,26 @@ export default class Gallery extends Component{
         show: false
     }
     componentDidMount(){
+        // show the page after 5 second
         setTimeout(() => this.setState({show: true}), 5000)
     }
+    // assign different class for different image width
     getImageWidth = (imgSrc) => {
         let newImg = new Image();
         newImg.src = imgSrc;
+        // passing class horizontal if image container size divide by image width is smaller than 4  if not dont pass anything
         return 1200 / newImg.width < 4 ?  'horizontal' : ''
       }
+    // assign different class for different image height
+
       getImageHeights = (imgSrc) => {
         let newImg = new Image();
         newImg.src = imgSrc;
+        // if image size /200 smaller was bigger than 10 pass class .vertical-10 or if not pass round of imagehight / 2 like .vertical-3
         let vertical = Math.round(newImg.height/200) > 10 ? 10 : Math.round(newImg.height/200);
         return `vertical-${vertical}`
       }
+
       getImageHeight = (imgSrc) => {
         let newImg = new Image();
         newImg.src = imgSrc;
@@ -28,6 +35,7 @@ export default class Gallery extends Component{
     render(){
         if(!this.state.show) {
             return(
+                // show loader before image load completed.
                 <div className="loader"><Icon loading name='spinner' size="massive"/></div>
             )
         }
@@ -36,14 +44,15 @@ export default class Gallery extends Component{
                 <div className='container'>
                 <div>
                     <ul>
+                        {/* top navigation */}
                         <li><Link to='/gallery'>Gallery</Link></li>
                         <li><Link to='/'>Home</Link></li>
                         <li><Link to='/second-gallery'>Second Gallery</Link></li>
                     </ul>
                     </div>
                     <div className='gallery'>
+                        {/* rendering images from list of images and getting different class name for different image size */}
                         {images.map((image, i) => {
-
                             return (
                                 <div key={i} className={`gallery_item ${this.getImageWidth(image.url)} ${this.getImageHeights(image.url)} `}>
                                   <img  alt="ss" src={image.url}/>
@@ -52,7 +61,6 @@ export default class Gallery extends Component{
                         })}
                     </div>
                 </div>
-            
         )
     }
 }
--- src/containers/secondGallery.js
+++ src/containers/secondGallery.js
@@ -8,18 +8,24 @@ export default class SecondGallery extends Component {
     show: false
   };
   componentDidMount() {
+    // load the page after 5 second
     setTimeout(() => this.setState({ show: true }), 5000);
   }
   getImageClass = imgSrc => {
+    // declaring the type of image which is horizontal or vertical or squre and then pass right class to the each image
+    // in this solution simply im adding one of the class big vertic or horizon to the image depends the conditions
     let newImg = new Image();
     newImg.src = imgSrc;
     const height = newImg.height;
     const width = newImg.width;
     if (height === width) {
+      // add class big if height and width is same size
       return "big";
     } else if (height > width) {
+      // add class vertic if height is bigger than width
       return "vertic";
     } else if (width > height) {
+      // add classs horizon if the width is bigger than height
       return "horizon";
     } else {
       return "";
@@ -28,6 +34,7 @@ export default class SecondGallery extends Component {
   render() {
     if (!this.state.show) {
       return (
+        // show loader before images completly load in the page.
         <div className="loader">
           <Icon loading name="spinner" size="massive" />
         </div>
@@ -51,6 +58,7 @@ export default class SecondGallery extends Component {
         <div className="second-gallery">
           {images.map((image, i) => {
             return (
+              // render images in DOM with different class depends the size of image
               <div key={i} className={`${this.getImageClass(image.url)}`}>
                 <img alt="ss" src={image.url} />
               </div>
